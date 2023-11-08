c412e88853c539b4af8b78394c89d5368854a226
Bruno Gallotte <brunogallotte1@gmail.com>
2023-11-08 17:13:26 -0300

    Fix: exercicio corrigido

--- estrutura-de-dados/testes.py
+++ estrutura-de-dados/testes.py
@@ -1,52 +1,56 @@
-filmes = {
-    '1': {
-        'nome':'Um sonho de Liberdade',
-        'ano': '1994',
-        'sinopse': 'lorem hahaha'
+# Organizei simulando um id para cada filme dentro do dicionário... só não coloquei a sinopse real para nāo tomar muito espaço...
+
+filmes = [
+    {
+    'nome':'Um sonho de Liberdade',
+    'ano': '1994',
+    'sinopse': 'lorem hahaha'
     },
-    '2': {
+    {
         'nome':'O poderoso chefāo',
         'ano': '1972',
         'sinopse': 'lorem hahaha'
     },
-    '3': {
+    {
         'nome':'Batman - O Cavaleiro das Trevas',
         'ano': '2008',
         'sinopse': 'lorem hahaha'
     },
-    '4': {
+    {
         'nome':'O poderoso Chefāo II',
         'ano': '1974',
         'sinopse': 'lorem hahaha'
     },
-    '5': {
+    {
         'nome':'12 Homens e uma Sentença',
         'ano': '1957',
         'sinopse': 'lorem hahaha'
     },
-    '6': {
+    {
         'nome':'A lista de Schindler',
         'ano': '1993',
         'sinopse': 'lorem hahaha'
     },
-    '7': {
+    {
         'nome':'O senhor dos Anéis: O retorno do Rei',
         'ano': '2003',
         'sinopse': 'lorem hahaha'
     },
-    '8': {
+    {
         'nome':'Pulp Fiction - Tempo de Violência',
         'ano': '1994',
         'sinopse': 'lorem hahaha'
     },
-    '9': {
+    {
         'nome':'O senhor dos Anéis: A sociedade do Anel',
         'ano': '2001',
         'sinopse': 'lorem hahaha'
     },
-    '10': {
+    {
         'nome':'Três homens em Conflito',
         'ano': '1966',
         'sinopse': 'lorem hahaha'
     }
-}
+]
+
+print(filmes)
